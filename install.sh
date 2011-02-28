06a2b30a3118ede54ed369abba3116524520383c
Koen Bok <koen+github@madebysofa.com>
2011-02-28 22:36:10 +0100

    Add pyfsevents dist

--- install.sh
+++ install.sh
@@ -2,9 +2,20 @@
 
 echo "Installing Cactus, hold on..."
 
+cd /tmp
+
+# Install dependencies
+
 easy_install baker django simplejson keyring boto pyfsevents
 
-cd /tmp
+# Install pyfsevents
+
+curl -O https://github.com/koenbok/Cactus/raw/master/vendor/pyfsevents.so.gz
+gzip -d pyfsevents.so.gz
+mv pyfsevents.so `python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`/pyfsevents.so
+
+# Install Cactus
+
 curl -O https://github.com/koenbok/Cactus/raw/master/cactus.py
 chmod 755 cactus.py
 mv cactus.py /usr/local/bin/cactus
