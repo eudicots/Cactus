bb291f9d391c7261e36b957557c346f9e1a38308
Koen Bok <koenbok@gmail.com>
2012-06-19 21:54:58 -0700

    Better tests

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,22 @@ cactus create /tmp/catus-test
 cd /tmp/catus-test
 
 cactus build
+
+echo '{% extends "base.html" %}
+{% block content %}
+Welcome to Cactus! It worked!
+{% endblock %}' > pages/index.html
+
 cactus serve
+
+echo '{
+    "aws-access-key": "AKIAIV5C2U4YJM6DRMRQ", 
+    "aws-bucket-name": "cactus-test-test", 
+    "aws-bucket-website": "cactus-test-test.s3-website-us-east-1.amazonaws.com"
+}' > config.json
+
+cactus deploy
+
+curl http://cactus-test-test.s3-website-us-east-1.amazonaws.com/versions.txt
 
 rm -Rf /tmp/catus-test
