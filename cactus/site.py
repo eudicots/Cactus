8331075b34072c1159bd8808b50babe7042973fe
Koen Bok <koenbok@gmail.com>
2012-09-10 23:02:45 -0700

    better bucket creation

--- cactus/site.py
+++ cactus/site.py
@@ -254,7 +254,15 @@ class Site(object):
 				
 				# Configure S3 to use the index.html and error.html files for indexes and 404/500s.
 				awsBucket.configure_website('index.html', 'error.html')
-			
+
+				self.config.set('aws-bucket-website', awsBucket.get_website_endpoint())
+				self.config.set('aws-bucket-name', awsBucketName)
+				self.config.write()
+
+				logging.info('Bucket %s was selected with website endpoint %s' % (self.config.get('aws-bucket-name'), self.config.get('aws-bucket-website')))
+				logging.info('You can learn more about s3 (like pointing to your own domain) here: https://github.com/koenbok/Cactus')
+
+
 			else: return
 		else:
 			
@@ -262,13 +270,11 @@ class Site(object):
 			for b in buckets:
 				if b.name == awsBucketName:
 					awsBucket = b
-
-		self.config.set('aws-bucket-website', awsBucket.get_website_endpoint())
-		self.config.set('aws-bucket-name', awsBucketName)
-		self.config.write()
-
-		logging.info('Bucket %s was selected with website endpoint %s' % (self.config.get('aws-bucket-name'), self.config.get('aws-bucket-website')))
-		logging.info('You can learn more about s3 (like pointing to your own domain) here: https://github.com/koenbok/Cactus')
+		
+		# Make sure we have the endpoint in the config
+		if not self.config.get('aws-bucket-website'):
+			self.config.set('aws-bucket-website', awsBucket.get_website_endpoint())
+			self.config.write()
 		
 		logging.info('Uploading site to bucket %s' % awsBucketName)
 		
