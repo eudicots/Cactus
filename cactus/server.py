df43fbda068d416203791c22b8da7ef2a9f42f5c
Koen Bok <koenbok@gmail.com>
2012-09-17 23:09:27 -0700

    Simpler logging

--- cactus/server.py
+++ cactus/server.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import SimpleHTTPServer
 import SocketServer
 
@@ -18,3 +21,14 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 		
 		return SimpleHTTPServer.SimpleHTTPRequestHandler.send_error(
 			self, code, message=None)
+	
+	def log_message(self, format, *args):
+		sys.stdout.write("%s\n" % format%args)
+
+	def log_request(self, code='', size=''):
+		try:
+			self.log_message('%s %s %s', str(code), 
+				self.requestline.split(' ')[0], 
+				self.requestline.split(' ')[1])
+		except:
+			pass
