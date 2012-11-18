5aa0d45a3fcfd25a953ff053bdb4455941d85afb
Koen Bok <koen@madebysofa.com>
2012-11-18 13:13:54 -0800

    - Add debug logging - Set global network timeout

--- cactus/__init__.py
+++ cactus/__init__.py
@@ -1,9 +1,23 @@
+import os
 import logging
+import json
+import socket
 
-logging.basicConfig(
-	format='%(message)s', 
-	level=logging.INFO
-)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.settimeout(5)
+
+logLevel = logging.INFO
+
+if os.environ.get('DEBUG'):
+	logging.basicConfig(
+		format='%(filename)s:%(lineno)s / %(levelname)s -> %(message)s', 
+		level=logging.DEBUG
+	)
+else:
+	logging.basicConfig(
+		format='%(message)s', 
+		level=logging.INFO
+	)
 
 from site import Site
 
