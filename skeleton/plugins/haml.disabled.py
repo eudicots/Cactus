e869c4fd36cdc8a5a0904f448cda23fa903a7d11
jochen brissier <codenameuser@me.com>
2012-12-06 09:01:10 +0100

    + how to header

--- skeleton/plugins/haml.disabled.py
+++ skeleton/plugins/haml.disabled.py
@@ -2,6 +2,11 @@ import sys
 import os
 import codecs
 
+# How to:
+#     * Install hamlpy (https://github.com/jessemiller/HamlPy)
+#     * .haml files will compiled to .html files
+
+
 from cactus.utils import fileList
 from hamlpy.hamlpy import Compiler
 
@@ -9,22 +14,22 @@ CLEANUP = []
 
 def preBuild(site):
     for path in fileList(site.paths['pages']):
-        
+
         #only file ends with haml
         if not path.endswith('.haml'):
             continue
-        
+
         #read the lines
         haml_lines = codecs.open(path, 'r', encoding='utf-8').read().splitlines()
-        
-        #compile haml to html 
+
+        #compile haml to html
         compiler = Compiler()
         output = compiler.process_lines(haml_lines)
-        
-        #replace path 
+
+        #replace path
         outPath = path.replace('.haml', '.html')
 
-        #write the html file 
+        #write the html file
         with open(outPath,'w') as f:
             f.write(output)
 
