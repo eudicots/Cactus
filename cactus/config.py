3130ec2f30b79a8e6966e0221dfc63991b80c30b
Thomas Orozco <thomas@orozco.fr>
2013-05-07 19:36:32 -0700

    Add logging when an error occurs with config

--- cactus/config.py
+++ cactus/config.py
@@ -16,9 +16,13 @@ class Config(object):
     def load(self):
         try:
             self._data = json.load(open(self.path, 'r'))
+        except IOError:
+            logging.info('No configuration file found at {0}'.format(self.path))
+            self._data = {}
         except Exception:
             logging.warning('Unable to load configuration at {0}'.format(self.path))
             self._data = {}
 
     def write(self):
         json.dump(self._data, open(self.path, 'w'), sort_keys = True, indent = 4)
+        logging.debug('Saved configuration at {0}'.format(self.path))
