09af63ce42bcd3a84cc40af51ad3f42115087b3d
Justin Hamade <justin@openroad.ca>
2013-05-09 16:50:15 -0700

    Added i18nlint from https://github.com/rory/django-template-i18n-lint with command line interface

--- cactus/cli.py
+++ cactus/cli.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from optparse import OptionParser
 
 import sys
 import os
@@ -7,91 +8,111 @@ import time
 
 import cactus
 
+
 def create(path):
-	"Creates a new project at the given path."
-	
-	if os.path.exists(path):
-		if raw_input('Path %s exists, move aside (y/n): ' % path) == 'y':
-			os.rename(path, '%s.%s.moved' % (path, int(time.time())))
-		else:
-			sys.exit()
-	
-	site = cactus.Site(path)
-	site.bootstrap()
+    "Creates a new project at the given path."
+
+    if os.path.exists(path):
+        if raw_input('Path %s exists, move aside (y/n): ' % path) == 'y':
+            os.rename(path, '%s.%s.moved' % (path, int(time.time())))
+        else:
+            sys.exit()
+
+    site = cactus.Site(path)
+    site.bootstrap()
 
 
 def build(path):
-	"Build a cactus project"
-	
-	site = cactus.Site(path)
-	site.verify()
-	site.build()
+    "Build a cactus project"
+
+    site = cactus.Site(path)
+    site.verify()
+    site.build()
+
 
 def serve(path, port=8000, browser=True):
-	"Serve the project and watch changes"
-	
-	site = cactus.Site(path)
-	site.verify()
-	site.serve(port=port, browser=browser)
+    "Serve the project and watch changes"
+
+    site = cactus.Site(path)
+    site.verify()
+    site.serve(port=port, browser=browser)
+
 
 def deploy(path):
-	"Upload the project to S3"
-	
-	site = cactus.Site(path)
-	site.verify()
-	site.upload()
+    "Upload the project to S3"
+
+    site = cactus.Site(path)
+    site.verify()
+    site.upload()
+
 
 def help():
-	print
-	print 'Usage: cactus [create|build|serve|deploy]'
-	print
-	print '    create: Create a new website skeleton at path'
-	print '    build: Rebuild your site from source files'
-	print '    serve <port>: Serve you website at local development server'
-	print '    deploy: Upload and deploy your site to S3'
-	print
+    print
+    print 'Usage: cactus [create|build|serve|deploy|i18nlint]'
+    print
+    print '    create: Create a new website skeleton at path'
+    print '    build: Rebuild your site from source files'
+    print '    serve <port>: Serve you website at local development server'
+    print '    deploy: Upload and deploy your site to S3'
+    print
+
 
 def exit(msg):
-	print msg
-	sys.exit()
+    print msg
+    sys.exit()
+
 
 def main():
-	
-	command = sys.argv[1] if len(sys.argv) > 1 else None
-	option1 = sys.argv[2] if len(sys.argv) > 2 else None
-	option2 = sys.argv[3] if len(sys.argv) > 3 else None
-	
-	# If we miss a command we exit and print help
-	if not command:
-		help()
-		sys.exit()
+    command = sys.argv[1] if len(sys.argv) > 1 else None
+    option1 = sys.argv[2] if len(sys.argv) > 2 else None
+    option2 = sys.argv[3] if len(sys.argv) > 3 else None
 
-	# Run the command
-	if command == 'create':
-		if not option1: exit('Missing path')
-		create(option1)
+    # If we miss a command we exit and print help
+    if not command:
+        help()
+        sys.exit()
 
-	elif command == 'build':
-		build(os.getcwd())
+    # Run the command
+    if command == 'create':
+        if not option1: exit('Missing path')
+        create(option1)
 
-	elif command == 'serve':
-		
-		if option1:
-			try: option1 = int(option1)
-			except: exit('port should be a round number like 5000, 8000, 8080')
-		else:
-			option1 = 8000
+    elif command == 'build':
+        build(os.getcwd())
 
-		browser = False if option2 == '-n' else True
+    elif command == 'serve':
 
-		serve(os.getcwd(), port=option1, browser=browser)
+        if option1:
+            try:
+                option1 = int(option1)
+            except:
+                exit('port should be a round number like 5000, 8000, 8080')
+        else:
+            option1 = 8000
 
-	elif command == 'deploy':
-		deploy(os.getcwd())
+        browser = False if option2 == '-n' else True
 
-	else:
-		print 'Unknown command: %s' % command
-		help()
+        serve(os.getcwd(), port=option1, browser=browser)
+
+    elif command == 'deploy':
+        deploy(os.getcwd())
+
+    elif command == 'i18nlint':
+        parser = OptionParser(usage="usage: %prog [options] <filename>")
+        parser.add_option("-r", "--replace", action="store_true", dest="replace",
+                help="Ask to replace the strings in the file.", default=False)
+        (options, args) = parser.parse_args()
+        if len(args) != 2:
+            parser.error("incorrect number of arguments")
+        from i18nlint import replace_strings, print_strings
+        if options.replace:
+            replace_strings(args[1])
+        else:
+            print_strings(args[1])
+
+    else:
+        print 'Unknown command: %s' % command
+        help()
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
---
+++ cactus/i18nlint.py
@@ -0,0 +1,137 @@
+#! /usr/bin/python
+"""
+Prints out all
+"""
+
+import re
+import sys
+from optparse import OptionParser
+
+
+def location(str, pos):
+    """Given a string str and an integer pos, find the line number and character in that line that correspond to pos"""
+    lineno, charpos = 1, 1
+    counter = 0
+    for char in str:
+        if counter == pos:
+            return lineno, charpos
+        elif char == '\n':
+            lineno += 1
+            charpos = 1
+            counter += 1
+        else:
+            charpos += 1
+            counter += 1
+
+    return lineno, charpos
+
+# Things that are OK:
+GOOD_STRINGS = re.compile(
+    r"""
+          # django comment
+       ( {%\ comment\ %}.*?{%\ endcomment\ %}
+
+         # already translated text
+        |{%\ ?blocktrans.*?{%\ ?endblocktrans\ ?%}
+
+         # any django template function (catches {% trans ..) aswell
+        |{%.*?%}
+
+         # CSS
+        |<style.*?</style>
+
+         # JS
+        |<script.*?</script>
+
+         # A html title or value attribute that's been translated
+        |(?:value|title|summary|alt)="{%\ ?trans.*?%}"
+
+         # A html title or value attribute that's just a template var
+        |(?:value|title|summary|alt)="{{.*?}}"
+
+         # An <option> value tag
+        |<option[^<>]+?value="[^"]*?"
+
+         # Any html attribute that's not value or title
+        |[a-z:-]+?(?<!alt)(?<!value)(?<!title)(?<!summary)='[^']*?'
+
+         # Any html attribute that's not value or title
+        |[a-z:-]+?(?<!alt)(?<!value)(?<!title)(?<!summary)="[^"]*?"
+
+         # HTML opening tag
+        |<[\w:]+
+
+         # End of a html opening tag
+        |>
+        |/>
+
+         # closing html tag
+        |</.*?>
+
+         # any django template variable
+        |{{.*?}}
+
+         # HTML doctype
+        |<!DOCTYPE.*?>
+
+         # IE specific HTML
+        |<!--\[if.*?<!\[endif\]-->
+
+         # HTML comment
+        |<!--.*?-->
+
+         # HTML entities
+        |&[a-z]{1,10};
+
+         # CSS style
+        |<style.*?</style>
+
+         # another common template comment
+        |{\#.*?\#}
+        )""",
+
+    # MULTILINE to match across lines and DOTALL to make . include the newline
+    re.MULTILINE|re.DOTALL|re.VERBOSE)
+
+# Stops us matching non-letter parts, e.g. just hypens, full stops etc.
+LETTERS = re.compile("\w")
+
+
+def replace_strings(filename):
+    full_text_lines = []
+    for index, message in enumerate(GOOD_STRINGS.split(open(filename).read())):
+        if index % 2 == 0 and re.search("\w", message):
+            before, message, after = re.match("^(\s*)(.*?)(\s*)$", message, re.DOTALL).groups()
+            message = message.strip().replace("\n", "").replace("\r", "")
+            change = raw_input("Make '%s' translatable? [Y/n] " % message)
+            if change == 'y' or change == "":
+                message = '%s{%% trans "%s" %%}%s' % (before, message, after)
+        full_text_lines.append(message)
+
+    full_text = "".join(full_text_lines)
+    save_filename = filename.split(".")[0] + "_translated.html"
+    open(save_filename, 'w').write(full_text)
+    print "Fully translated! Saved as: %s" % save_filename
+
+
+def non_translated_text(filename):
+
+    template = open(filename).read()
+    offset = 0
+
+    # Find the parts of the template that don't match this regex
+    # taken from http://www.technomancy.org/python/strings-that-dont-match-regex/
+    for index, match in enumerate(GOOD_STRINGS.split(template)):
+        if index % 2 == 0:
+
+            # Ignore it if it doesn't have letters
+            if LETTERS.search(match):
+                lineno, charpos = location(template, offset)
+                yield (lineno, charpos, match.strip().replace("\n", "").replace("\r", "")[:120])
+
+        offset += len(match)
+
+
+def print_strings(filename):
+    for lineno, charpos, message in non_translated_text(filename):
+        print "%s:%s:%s:%s" % (filename, lineno, charpos, message)
