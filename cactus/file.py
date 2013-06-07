39332e5f483042689c5711d60a0739227e227ee2
Thomas Orozco <thomas@orozco.fr>
2013-06-07 17:22:38 +0900

    Compress whenever there is a gain

--- cactus/file.py
+++ cactus/file.py
@@ -13,20 +13,27 @@ import copy
 class File(object):
     DEFAULT_CACHE_EXPIRATION = 60 * 60 * 24 * 7  # One week
     COMPRESS_TYPES = ['html', 'css', 'js', 'txt', 'xml']
-    COMPRESS_MIN_SIZE = 1024  # 1kb
     PROGRESS_MIN_SIZE = (1024 * 1024) / 2  # 521 kb
 
     def __init__(self, site, path):
         self.site = site
         self.path = path
+
         self.force_refresh = False
+        self._is_compressed = None
+
+    def prepare(self):
+        """
+        Prepare the file for upload
+        """
+        self.payload()  # Decide whether we'll compress or not.
+        self.lastUpload = 0
 
     def cache_duration(self):
         if self.site.cache_duration is not None:
             return self.site.cache_duration
         return self.DEFAULT_CACHE_EXPIRATION
 
-    @memoize
     def data(self):
         with open(os.path.join(self.site.path, '.build', self.path)) as f:
             return f.read()
@@ -37,12 +44,30 @@ class File(object):
         server. This might be compressed based on the content type and size.
         """
         if not hasattr(self, '_payload'):
-            if self.shouldCompress():
-                self._payload = compressString(self.data())
-            else:
-                self._payload = self.data()
+            raw_data = self.data()
+
+            if self.extension() in self.COMPRESS_TYPES:
+                compressed_data = compressString(raw_data)
+
+                if len(compressed_data) < len(raw_data):
+                    self._is_compressed = True
+                    self._payload = compressed_data
+                    return self._payload
+
+            self.is_compressed = False
+            self._payload = raw_data
 
         return self._payload
+
+    @property
+    def is_compressed(self):
+        if self._is_compressed is None:
+            raise Exception('Compression not defined yet!')
+        return self._is_compressed
+
+    @is_compressed.setter
+    def is_compressed(self, boolean):
+        self._is_compressed = boolean
 
     def checksum(self):
         """
@@ -78,7 +103,7 @@ class File(object):
             return True
 
         remote_headers = {k: v.strip('"') for k, v in getURLHeaders(self.remoteURL()).items()}
-        local_headers = copy.copy(self.headers)  # Will do a copy.
+        local_headers = copy.copy(self.headers)
         local_headers['etag'] = self.checksum()
         for k, v in local_headers.items():  # Don't check AWS' own headers.
             if remote_headers.get(k) != v:
@@ -87,11 +112,11 @@ class File(object):
 
     @retry(socket.error, tries=5, delay=3, backoff=2)
     def upload(self, bucket):
-        self.lastUpload = 0
+        self.prepare()
 
         self.headers = CaseInsensitiveDict((('Cache-Control', 'max-age=%s' % self.cache_duration()),))
 
-        if self.shouldCompress():
+        if self.is_compressed:
             self.headers['Content-Encoding'] = 'gzip'
 
         self.site.plugin_manager.preDeployFile(self)
@@ -125,7 +150,7 @@ class File(object):
                                          num_cb=progressCallbackCount)
 
         op1 = '+' if changed else '-'
-        op2 = ' (%s compressed)' % (fileSize(len(self.payload()))) if self.shouldCompress() else ''
+        op2 = ' (%s compressed)' % (fileSize(len(self.payload()))) if self.is_compressed else ''
 
         logging.info('%s %s - %s%s' % (op1, self.path, fileSize(len(self.data())), op2))
 
