ea0dd7a24fd512bac16749b87354e2b888cb96c2
Thomas Orozco <thomas@orozco.fr>
2013-07-03 10:09:20 -0700

    Added {% current_page %} template tag

--- cactus/template_tags.py
+++ cactus/template_tags.py
@@ -9,6 +9,7 @@ def static(context, link_url):
     Get the path for a static file in the Cactus build.
     We'll need this because paths can be rewritten with fingerprinting.
     """
+    #TODO: Support URLS that don't start with `/static/`
     return context['__CACTUS_SITE__'].get_url_for_static(link_url)
 
 
@@ -26,16 +27,26 @@ def url(context, link_url):
     return url
 
 
+def current_page(context):
+    """
+    Returns the current URL
+    """
+    page = context['__CACTUS_CURRENT_PAGE__']
+
+    return page.final_url
+
+
 def if_current_page(context, link_url, positive=True, negative=False):
     """
     Return one of the passed parameters if the URL passed is the current one.
     For consistency reasons, we use the link_url of the page.
     """
-    current_page = context['__CACTUS_CURRENT_PAGE__']
+    page = context['__CACTUS_CURRENT_PAGE__']
 
-    return positive if current_page.link_url == link_url else negative
+    return positive if page.link_url == link_url else negative
 
 
 register.simple_tag(takes_context=True)(static)
 register.simple_tag(takes_context=True)(url)
+register.simple_tag(takes_context=True)(current_page)
 register.simple_tag(takes_context=True)(if_current_page)
