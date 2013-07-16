aff14106ea062b95e6a10161dbcb3b8e23fb4b4d
Thomas Orozco <thomas@orozco.fr>
2013-07-15 23:12:48 -0700

    Remove password file
    Superseded by the keyring module

--- cactus/deployment/s3/auth.py
+++ cactus/deployment/s3/auth.py
@@ -1,7 +1,6 @@
 #coding:utf-8
-import getpass
 from cactus.deployment.auth import BaseKeyringCredentialsManager
-from cactus.utils import password
+
 
 class AWSCredentialsManager(BaseKeyringCredentialsManager):
     _username_config_entry = "aws-access-key"
