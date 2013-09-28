b95175bb9dcef5067b5a3b0edf8623d8c06dc07a
Koen Bok <koen@madebysofa.com>
2013-09-28 12:41:59 +0200

    Better coffeescript plugin

--- cactus/skeleton/plugins/coffeescript.disabled.py
+++ cactus/skeleton/plugins/coffeescript.disabled.py
@@ -1,8 +1,28 @@
 import os
 import pipes
+import subprocess
+import logging
 
-os.environ['PATH'] = '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/share/npm/bin:'
+def run(command):
+    
+    logger = logging.getLogger(__name__)
+    
+    logger.debug(command)
+    
+    os.environ['PATH'] = '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/share/npm/bin:'
+    
+    process = subprocess.Popen([command], 
+        shell=True, 
+        stdin=subprocess.PIPE, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE)
+    
+    stdout = process.stdout.readline()
+    stderr = process.stderr.readline()
+    
+    if stdout: logger.info(stdout)
+    if stderr: logger.warning(stderr)
 
-def postBuild(site):
-	command = 'coffee -c %s/static/js/*.coffee' % pipes.quote(site.paths['build'])
-	os.system(command)
+
+def preBuild(site):
+    run('coffee -c %s/js/*.coffee' % pipes.quote(site.static_path))
