410ad8851c65d0422c094f12a8b84e66bce5b874
Koen Bok <koen@madebysofa.com>
2013-09-28 12:42:48 +0200

    Add standard logging to the server, throw warnings on 404. Suppress broken pipe messages.

--- cactus/server.py
+++ cactus/server.py
@@ -1,19 +1,32 @@
 import os
 import sys
+import logging
+import errno
 
 import SimpleHTTPServer
 import SocketServer
 
 import mime
 
+logger = logging.getLogger(__name__)
+
 # See: https://github.com/koenbok/Cactus/issues/8
 # class Server(SocketServer.ForkingMixIn, SocketServer.TCPServer):
 #	allow_reuse_address = True
 
-
-class Server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+class Server(SocketServer.ThreadingMixIn, SocketServer.TCPServer, object):
+    
     allow_reuse_address = True
 
+    def handle_error(self, request, client_address):
+
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        
+        # Don't complain about agressive browsers all the time
+        if "Broken pipe" in exc_obj:
+            return
+        
+        return super(Server, self).handle_error(request, client_address)
 
 class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def send_head(self):
@@ -57,8 +70,8 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
             if os.path.exists(errorPagePath):
                 return self.send_content(404,
-                                         {"Content-type": "text/html"},
-                                         open(errorPagePath, 'rb'))
+                    {"Content-type": "text/html"}, 
+                    open(errorPagePath, 'rb'))
             else:
                 self.send_error(404, "File not found")
             return None
@@ -71,7 +84,8 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             "Content-Length": str(fs[6]),
             "Cache-Control": "no-cache, must-revalidate",
         }
-
+        
+        # It would be great if this worked reliably, but for now we use no caching
         # Last-Modified", self.date_time_string(fs.st_mtime)
 
         return self.send_content(200, headers, f)
@@ -91,12 +105,14 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         sys.stdout.write("%s\n" % fmt % args)
 
     def log_request(self, code = '', size = ''):
-        try:
-            self.log_message('%s %s %s', str(code),
-                             self.requestline.split(' ')[0],
-                             self.requestline.split(' ')[1])
-        except Exception:
-            pass
+        
+        action = self.requestline.split(' ')[0]
+        path = self.requestline.split(' ')[1]
+        
+        if code in [200]:
+            logger.info('%s %s %s', str(code), action, path)
+        else:
+            logger.warning('%s %s %s', str(code), action, path)
 
     def guess_type(self, path):
         return mime.guess(path)
