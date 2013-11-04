e5d66daa060ce94fdb586993496080b84867d8a9
Koen Bok <koen@madebysofa.com>
2013-11-04 11:52:58 +0100

    Test custom header config

--- cactus/tests/test_context.py
+++ cactus/tests/test_context.py
@@ -39,3 +39,28 @@ class TestSiteContext(SiteTestCase):
 
         self.assertEqual(a, "1")
         self.assertEqual(b, "True")
+
+class TestCustomPageContext(SiteTestCase):
+    """
+    Test that custom context in the header of pages is feeded to a page
+
+    Includes the built-in site context ('CACTUS'), and custom context.
+    """
+    def setUp(self):
+        super(TestCustomPageContext, self).setUp()
+
+        page = "a: 1\n\tb: Monkey\n{{ a }}\n{{ b }}"
+        with open(os.path.join(self.site.page_path, "test.html"), "w") as f:
+            f.write(page)
+
+        self.site.build()
+
+    def test_custom_context(self):
+        """
+        Test that custom context is provided
+        """
+        with open(os.path.join(self.site.build_path, "test.html")) as f:
+            a, b = f.read().split("\n")
+
+        self.assertEqual(a, "1")
+        self.assertEqual(b, "Monkey")
