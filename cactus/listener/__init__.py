63f8f15d359fd12827ef41def4faf72a5e998640
Thomas Orozco <thomas@orozco.fr>
2015-10-10 09:16:29 +0200

    Make FSEventsListener a debug-only thing

--- cactus/listener/__init__.py
+++ cactus/listener/__init__.py
@@ -10,5 +10,5 @@ logger = logging.getLogger(__name__)
 try:
     from cactus.listener.mac import FSEventsListener as Listener
 except (ImportError, OSError):
-    logger.warning("Failed to load FSEventsListener", exc_info=True)
+    logger.debug("Failed to load FSEventsListener, falling back to PollingListener", exc_info=True)
     Listener = PollingListener
