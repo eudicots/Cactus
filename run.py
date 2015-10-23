1ccfd82a7c3e68e2ccd46025961e67a4970f0cf6
Christian Haudum <christian.haudum@gmail.com>
2015-10-23 20:12:43 +0200

    fix run.py script

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import sys
 import cactus
 
 from cactus.cli import main
 
-print "Using: %s" % cactus.__file__
+print("Using: {0}".format(cactus.__file__))
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
+
